1bb9a4da3279b088f40542bd50b5d60b1189f185
Santozz06 <felipekauan790@gmail.com>
2025-08-25 16:42:08 -0300

    Resolvido o problema com o layout bugado dos radio

--- veiculos/veiculos_create.php
+++ veiculos/veiculos_create.php
@@ -7,24 +7,50 @@ include("../includes/conexao.php");
 <div class="container">
     <h2>Cadastro de Veículo</h2>
     <form action="veiculos_save.php" method="POST">
-        Modelo: <input type="text" name="modelo" required>
-        Marca:
-        <select name="id_marca" required>
-            <option value="">Selecione a marca</option>
-            <?php
-            $sql = "SELECT * FROM marcas";
-            $marcas = mysqli_query($con, $sql);
-            while($m = mysqli_fetch_assoc($marcas)){
-                echo "<option value='".$m['id']."'>".$m['marca']."</option>";
-            }
-            ?>
-        </select>
-        Potência: <input type="number" name="potencia" required>
-        Ano: <input type="number" name="ano" required>
-        Tipo:
-        <label><input type="radio" name="tipo" value="Carro" required> Carro</label>
-        <label><input type="radio" name="tipo" value="Moto"> Moto</label>
-        <label><input type="radio" name="tipo" value="Caminhão"> Caminhão</label>
+        <div class="form-group">
+            <label>Modelo:</label>
+            <input type="text" name="modelo" required>
+        </div>
+        
+        <div class="form-group">
+            <label>Marca:</label>
+            <select name="id_marca" required>
+                <option value="">Selecione a marca</option>
+                <?php
+                $sql = "SELECT * FROM marcas";
+                $marcas = mysqli_query($con, $sql);
+                while($m = mysqli_fetch_assoc($marcas)){
+                    echo "<option value='".$m['id']."'>".$m['marca']."</option>";
+                }
+                ?>
+            </select>
+        </div>
+        
+        <div class="form-group">
+            <label>Potência:</label>
+            <input type="number" name="potencia" required>
+        </div>
+        
+        <div class="form-group">
+            <label>Ano:</label>
+            <input type="number" name="ano" required>
+        </div>
+        
+        <div class="form-group">
+            <label>Tipo:</label>
+            <div class="radio-group">
+                <label class="radio-option">
+                    <input type="radio" name="tipo" value="Carro" required> Carro
+                </label>
+                <label class="radio-option">
+                    <input type="radio" name="tipo" value="Moto"> Moto
+                </label>
+                <label class="radio-option">
+                    <input type="radio" name="tipo" value="Caminhão"> Caminhão
+                </label>
+            </div>
+        </div>
+        
         <input type="submit" value="Cadastrar" class="btn">
     </form>
 </div>
--- veiculos/veiculos_edit.php
+++ veiculos/veiculos_edit.php
@@ -13,24 +13,54 @@ $veiculo = mysqli_fetch_assoc($result);
     <h2>Editar Veículo</h2>
     <form action="veiculos_save.php" method="POST">
         <input type="hidden" name="id" value="<?php echo $veiculo['id']; ?>">
-        Modelo: <input type="text" name="modelo" value="<?php echo $veiculo['modelo']; ?>" required>
-        Marca:
-        <select name="id_marca" required>
-            <option value="">Selecione a marca</option>
-            <?php
-            $marcas = mysqli_query($con, "SELECT * FROM marcas");
-            while($m = mysqli_fetch_assoc($marcas)){
-                $selected = ($m['id'] == $veiculo['id_marca']) ? "selected" : "";
-                echo "<option value='".$m['id']."' $selected>".$m['marca']."</option>";
-            }
-            ?>
-        </select>
-        Potência: <input type="number" name="potencia" value="<?php echo $veiculo['potencia']; ?>" required>
-        Ano: <input type="number" name="ano" value="<?php echo $veiculo['ano']; ?>" required>
-        Tipo:
-        <label><input type="radio" name="tipo" value="Carro" <?php if($veiculo['tipo']=="Carro") echo "checked"; ?>> Carro</label>
-        <label><input type="radio" name="tipo" value="Moto" <?php if($veiculo['tipo']=="Moto") echo "checked"; ?>> Moto</label>
-        <label><input type="radio" name="tipo" value="Caminhão" <?php if($veiculo['tipo']=="Caminhão") echo "checked"; ?>> Caminhão</label>
+        
+        <div class="form-group">
+            <label>Modelo:</label>
+            <input type="text" name="modelo" value="<?php echo $veiculo['modelo']; ?>" required>
+        </div>
+        
+        <div class="form-group">
+            <label>Marca:</label>
+            <select name="id_marca" required>
+                <option value="">Selecione a marca</option>
+                <?php
+                $marcas = mysqli_query($con, "SELECT * FROM marcas");
+                while($m = mysqli_fetch_assoc($marcas)){
+                    $selected = ($m['id'] == $veiculo['id_marca']) ? "selected" : "";
+                    echo "<option value='".$m['id']."' $selected>".$m['marca']."</option>";
+                }
+                ?>
+            </select>
+        </div>
+        
+        <div class="form-group">
+            <label>Potência:</label>
+            <input type="number" name="potencia" value="<?php echo $veiculo['potencia']; ?>" required>
+        </div>
+        
+        <div class="form-group">
+            <label>Ano:</label>
+            <input type="number" name="ano" value="<?php echo $veiculo['ano']; ?>" required>
+        </div>
+        
+        <div class="form-group">
+            <label>Tipo:</label>
+            <div class="radio-group">
+                <label class="radio-option">
+                    <input type="radio" name="tipo" value="Carro" <?php if($veiculo['tipo']=="Carro") echo "checked"; ?> required> 
+                    Carro
+                </label>
+                <label class="radio-option">
+                    <input type="radio" name="tipo" value="Moto" <?php if($veiculo['tipo']=="Moto") echo "checked"; ?>> 
+                    Moto
+                </label>
+                <label class="radio-option">
+                    <input type="radio" name="tipo" value="Caminhão" <?php if($veiculo['tipo']=="Caminhão") echo "checked"; ?>> 
+                    Caminhão
+                </label>
+            </div>
+        </div>
+        
         <input type="submit" value="Salvar" class="btn">
     </form>
 </div>
